9b1675bb6478366d985897a29f239f3ca149f6f1
3vdi <unitcomics.world@proton.me>
2025-02-19 16:57:39 +0530

    first

---
+++ src/app/components/Window.jsx
@@ -0,0 +1,7 @@
+export default function Window(){
+    return(
+        <div className="w-[8ovw] h-[8ovh] bg-gray-200 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20">
+            <h1>hello</h1>
+        </div>
+    )
+}
