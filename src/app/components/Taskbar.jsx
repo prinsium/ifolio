dd44347fcf3d8d5ca7647e09011dc03f0758a6f9
3vdi <unitcomics.world@proton.me>
2025-02-19 17:03:56 +0530

    taskbar

--- src/app/components/Taskbar.jsx
+++ src/app/components/Taskbar.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Taskbar(){
     return(
-        <div className="w-full">
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 shadow-lg w-fit h-fit justify-center items-center">
             <div className="w-fit flex flex-row justify-around items-center bg-gray-700 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20">
                 <div className="p-4 gap-4 flex flex-row justify-around items-center">
                 <Image className="hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer" src="/web/63.svg" width={75} height={75} alt="Start" />
--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,7 @@ export default function Home() {
   return (
     <div className="w-screen h-screen">
       <Window />
-      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 shadow-lg w-fit h-fit justify-center items-center">
       <Taskbar />
-      </div>
     </div>
   );
 }
